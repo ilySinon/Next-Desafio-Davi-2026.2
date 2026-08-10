938538d6af05fc0a4ce11142cd06d2fccd4ec1fd
Davi <dahencosan@gmail.com>
2026-08-09 21:03:39 -0300

    feat: adicionando página de layout das páginas gerais do site.

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Inter, Anton } from "next/font/google";
+import "../globals.css";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const anton = Anton({
+  weight:"400",
+  variable: "--font-anton",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Loja Tricolor",
+  description: "Loja de vendas de produtos do Fluminense",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${inter.variable} ${anton.variable} antialiased`}
+      >
+        <Navbar/>
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  );
+}
