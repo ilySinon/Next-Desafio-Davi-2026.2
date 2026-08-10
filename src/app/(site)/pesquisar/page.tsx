364d753294b50a346f3ef4cb927b0f0ead636ebe
Davi <dahencosan@gmail.com>
2026-08-09 21:15:37 -0300

    feat: criando componente para página de pesquisa.

---
+++ src/app/(site)/pesquisar/page.tsx
@@ -0,0 +1,7 @@
+export default function PaginaPesquisa (){
+    return(
+        <div className=" space-y-24 md: space-y-32 w-full md: w-10/12 py-8 px-4 md:p-0 min-h-screen mx-auto bg-gray-600">
+            
+        </div>
+    )
+}
