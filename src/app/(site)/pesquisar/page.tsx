d178080d115a7df9476f8a786c6a079ccbd896fd
Davi <dahencosan@gmail.com>
2026-08-19 06:50:49 -0300

    feat: página de pesquisa é dinâmica e tem backend implementado.

--- src/app/(site)/pesquisar/page.tsx
+++ src/app/(site)/pesquisar/page.tsx
@@ -1,7 +1,31 @@
-export default function PaginaPesquisa (){
-    return(
-        <div className=" space-y-24 md: space-y-32 w-full md: w-10/12 py-8 px-4 md:p-0 min-h-screen mx-auto bg-gray-600">
-            
-        </div>
-    )
+import { TituloPesquisar } from "../../../components/PesquisarPagina/TituloPesquisar";
+import { CardsPesquisar } from "../../../components/PesquisarPagina/CardsPesquisar";
+import { filtrarPesquisaProdutos } from "@/actions/pesquisar/actions";
+
+export default async function PaginaPesquisa({
+    searchParams,
+}: {
+    searchParams: Promise<{
+        query?: string;
+        page?: string;
+    }>
+}) {
+
+    const params = await searchParams;
+    const query = params?.query || '';
+    const currentPage = Number(params?.page) || 1
+
+  const {produtos, count, totalPages} = await filtrarPesquisaProdutos(query, currentPage)
+
+  return (
+    <main className="w-full bg-(--var-Creme) min-h-screen pt-24 lg:pt-32 pb-16 px-6 lg:px-12">
+      <div className="max-w-7xl mx-auto w-full flex flex-col gap-8">
+        
+        <TituloPesquisar termo={query} quantidade={count} />
+        
+        <CardsPesquisar titulo={query} produtos={produtos} totalPages={totalPages} />
+        
+      </div>
+    </main>
+  );
 }
