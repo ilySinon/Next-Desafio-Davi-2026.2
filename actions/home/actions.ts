cfdf5825e5f5dbd66c7997a716ecdbfdf80f403f
Davi <dahencosan@gmail.com>
2026-08-19 06:32:31 -0300

    feat: criando action que é usado para o backend do carrosel na LandingPage.

---
+++ actions/home/actions.ts
@@ -0,0 +1,22 @@
+"use server"
+
+import prisma from "@/src/lib/db"
+
+export default async function getProdutosCarrossel() {
+  const produtos = await prisma.produto.findMany({
+    include: {
+      categoria: true,
+      variacoes: {
+        include: {
+          imagens: true,
+          estoques: true,
+        }
+      }
+    },
+      take: 6
+  });
+
+  return produtos;
+}
+
+
