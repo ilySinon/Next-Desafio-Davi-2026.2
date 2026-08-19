c9db1be2167b3d910256d4af3ba92220cfda439f
Davi <dahencosan@gmail.com>
2026-08-19 06:33:00 -0300

    feat: criando action que é usado para o backend doa pesquisa e paginação.

---
+++ actions/pesquisar/actions.ts
@@ -0,0 +1,38 @@
+"use server"
+
+import prisma from "@/src/lib/db"
+
+const itemsPorPag = 6;
+
+export async function filtrarPesquisaProdutos(query: string, currentPage: number){
+
+    const offset = (currentPage - 1) * itemsPorPag
+    const produtos = await prisma.produto.findMany({
+        where: {
+            OR: [{nomeProduto: {contains: query, mode: "insensitive"}}]
+        },
+        include: {
+        categoria: true,
+        variacoes: {
+            include: {
+            imagens: true,
+            estoques: true,
+            }
+        },
+        },
+        orderBy: {
+            nomeProduto: "asc"
+        },
+        take: itemsPorPag,
+        skip: offset
+    });
+
+    const count = await prisma.produto.count({
+        where:{
+            OR: [{nomeProduto: {contains: query, mode: "insensitive"}}]
+        },
+    });
+    const totalPages = Math.ceil(count / itemsPorPag); 
+
+    return { produtos, count, totalPages};
+}
