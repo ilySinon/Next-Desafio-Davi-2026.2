0d776fa28bc1020c796f596800f770a15fd92613
Davi <dahencosan@gmail.com>
2026-08-23 20:53:50 -0300

    fix: colocando filtro de mais recentes no carrossel.

--- actions/home/actions.ts
+++ actions/home/actions.ts
@@ -4,6 +4,9 @@ import prisma from "@/src/lib/db"
 
 export default async function getProdutosCarrossel() {
   const produtos = await prisma.produto.findMany({
+    orderBy:{
+      lancamento: 'desc'
+    },
     include: {
       categoria: true,
       variacoes: {
