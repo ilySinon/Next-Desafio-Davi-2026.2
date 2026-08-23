df6e5e4ce7fe7c625ea84f980d3c36ef29d4d7f1
Davi <dahencosan@gmail.com>
2026-08-23 20:52:18 -0300

    fix: arrumando o getProdutosTabela.

--- actions/dashboard/actions.ts
+++ actions/dashboard/actions.ts
@@ -2,8 +2,15 @@
 
 import prisma from "@/src/lib/db"
 
-export default async function getProdutosTabela() {
+const itemsPorTabela = 5;
+
+export default async function getProdutosTabela(query: string, currentPage: number) {
+  const offset = (currentPage - 1) * itemsPorTabela;
+
   const produtos = await prisma.produto.findMany({
+     where: {
+            OR: [{nomeProduto: {contains: query, mode: "insensitive"}, produtoDescricao: {contains: query, mode: "insensitive"}}]
+        },
     include: {
       categoria: true,
       variacoes: {
@@ -13,9 +20,16 @@ export default async function getProdutosTabela() {
         }
       }
     },
+    take: itemsPorTabela,
+    skip: offset,
+    orderBy: {
+      nomeProduto: "asc"
+    }
   });
 
-  return produtos;
+  const count = await prisma.produto.count();
+
+  const totalPages = Math.ceil(count / itemsPorTabela); 
+
+  return { produtos, count, totalPages };
 }
-
-
