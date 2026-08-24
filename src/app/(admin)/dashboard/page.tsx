0fedda6625cf88427893bce782dbe78e2b194770
Davi <dahencosan@gmail.com>
2026-08-23 21:01:54 -0300

    fix: deixando a tabela de produtos na forma final.

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,10 +1,24 @@
 import { TituloProdutos } from "../../../components/TabelaProdutosComponentes/TituloProdutos";
 import { TabelaProdutos } from "../../../components/TabelaProdutosComponentes/TabelaProdutos";
 import getProdutosTabela from "@/actions/dashboard/actions";
+import { obterTodasCategorias } from "../../../../actions/listarCategorias/action";
 
-export default async function GerenciamentoProdutos() {
-  const produtos = await getProdutosTabela();
-
+export default async function GerenciamentoProdutos({
+  searchParams,
+}: {
+    searchParams: Promise<{
+      query?: string;
+      page?: string;
+    }>
+} 
+) {
+  const params = await searchParams;
+  const currentPag = Number(params?.page) || 1;
+  const query = params?.query || '';
+  
+  const { produtos, count, totalPages } = await getProdutosTabela(query, currentPag);
+  const categorias = await obterTodasCategorias();
+  
   return (
     <main className="w-full min-h-screen bg-(--var-Creme) p-6 lg:p-12">
       <div className="max-w-7xl mx-auto w-full pt-20 lg:pt-0">
@@ -12,7 +26,12 @@ export default async function GerenciamentoProdutos() {
           titulo="Gerenciamento de Produtos" 
           descricao="Catálogo de camisas oficiais da loja tricolor." 
         />
-        <TabelaProdutos produtos={produtos}/>
+        <TabelaProdutos 
+          produtos={produtos} 
+          totalDeProdutos={count} 
+          totalPages={totalPages} 
+          categorias={categorias}
+        />
       </div>
     </main>
   );
